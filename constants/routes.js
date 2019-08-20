31ebd5576e3b786b5e44bf85d1df2f66c4c68cf0
Alban Nurkollari <albannurkollari@gmail.com>
2019-08-20 07:21:15 +0200

    Trying new things

--- constants/routes.js
+++ constants/routes.js
@@ -4,9 +4,11 @@ const DEFAULT_PATH = '/';
 // Generic
 const CREATE = `${DEFAULT_PATH}:create`;
 const ID = `${DEFAULT_PATH}:id`;
+const LOGIN = `${DEFAULT_PATH}:login`;
 
 module.exports = {
   DEFAULT_PATH,
   CREATE,
-  ID
+  ID,
+  LOGIN
 };
--- routes/account.js
+++ routes/account.js
@@ -1,8 +1,11 @@
+// Libraries
+const bcrypt = require('bcryptjs');
+
 // Mongoose schemas
 const Account = require('../models/account');
 
 // Constants
-const {DEFAULT_PATH, CREATE, ID} = require('../constants/routes');
+const {DEFAULT_PATH, CREATE, ID, LOGIN} = require('../constants/routes');
 
 
 // Routes
@@ -16,11 +19,45 @@ module.exports = [
     PUT: async ({body}, res) => res.json(await Account(body).save())
   }],
   [CREATE, {
-    POST: async ({body}, res) => res.json(await new Account({
-      firstName: body.firstName,
-      lastName: body.lastName,
-      email: body.email,
-      password: body.password
-    }).save())
+    POST: async ({body}, res) => {
+      if (await Account.findOne({email: body.email})) {
+        res.status(400).send('The email is already registered to an account!');
+
+        return;
+      }
+
+      const salt = await bcrypt.genSalt(); // => salt rounds (defaults to 10)
+      const password = await bcrypt.hash(body.password, salt);
+
+      const newAccount = new Account({
+        firstName: body.firstName,
+        lastName: body.lastName,
+        email: body.email,
+        password
+      });
+
+      res.json(await newAccount.save());
+    }
+  }],
+  [LOGIN, {
+    POST: async ({body}, res) => {
+      console.log(body);
+      const account = await Account.findOne({email: body.email});
+
+      if (!account) {
+        res.status(400).send('There is no account associated with this email address!');
+
+        return;
+      }
+
+      // Invalid password check
+      if (!(await bcrypt.compare(body.password, account.password))) {
+        res.status(400).send('Invalid password!');
+
+        return;
+      }
+
+      res.json({isLoggedIn: true});
+    }
   }]
 ];
